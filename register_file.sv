63460cab12b6a0093629df6f3d1c4d623cf8dbdc
Prakaash Karthikeyan <prakaashkarthik@gmail.com>
2019-05-12 16:18:08 -0400

    Updated RF to have 2 read ports and 1 write port; Added RF mnemonic spec; Updated CheckoutSpec with RF design decision

--- register_file.sv
+++ register_file.sv
@@ -1,37 +1,50 @@
+// TODO: Move this to 2 register reads and 1 register write
+// TODO: Match register file implementation to RV32I ISA spec
+
+// Need to mnemonic-fy the register indices. Defines or parameters? Parameter
+// doesn't make sense -- should probably be defines and included in. 
+
+// Also probably need to port out my entire register file so that I can debug
+// it? Need to see how I can dump this module in Verilator
+
 module register_file # (parameter REG_FILE_SIZE = 32) (
 	input clk, 
 	input rst_n, 
-	input [5:0] rd_reg_num, 
-	input [5:0] wr_reg_num, 
+	input [4:0] rd_reg_num0, 
+	input [4:0] rd_reg_num1, 
+	input [4:0] wr_reg_num, 
 	input write_en,
 	input [31:0] write_data,	
 	
-	output [31:0] read_data
+	output [31:0] read_data0, 
+	output [31:0] read_data1
 );
 
 
 	logic [31:0] reg_file [REG_FILE_SIZE];
+	logic final_write_en; 
 
-	// Reset logic
+	// Reset and write logic
+	
+	// According to RV32I spec, register 0 is the zero register and cannot
+	// be written to
+	assign final_write_en = (wr_reg_num == 0) ? 0 : write_en;
 	genvar i; 
 	generate 
 		for (i = 0; i < REG_FILE_SIZE; i++) begin
-			always_ff @(negedge rst_n) begin
+			always_ff @(posedge clk or negedge rst_n) begin
 				if (~rst_n) begin
 					reg_file[i] <= 0;
+				end else if (final_write_en) begin
+					reg_file[wr_reg_num] <= write_data;
 				end
 			end
 		end
 	endgenerate
 
 	// Read logic
-	assign read_data = reg_file[rd_reg_num];
+	assign read_data0 = reg_file[rd_reg_num0];
+	assign read_data1 = reg_file[rd_reg_num1];
 
-	// Write logic
-	always_ff @(posedge clk) begin
-		if (write_en) begin
-			reg_file[wr_reg_num] <= write_data;
-		end 
-	       // Do I need an else here for synthesizability?	
-	end
+
 endmodule
---
+++ register_file_defines.svh
@@ -0,0 +1,69 @@
+// Function specific register mnemnonics
+`define ZERO 0
+`define RA 1 // Return Address
+`define SP 2 // Stack Pointer
+`define GP 3 // Global Pointer
+`define TP 4 // Thread Pointer
+`define T0 5 // Temporary 0
+`define T1 6 // Temporary 1
+`define T2 7 // Temporary 2
+`define FP 8 // Frame Pointer
+`define S0 8 // Saved register 0
+`define S1 9 // Saved register 1
+`define A0 10 // Function argument / return value
+`define A1 11 // Function argument / return value
+`define A2 12 // Function argument
+`define A3 13 
+`define A4 14
+`define A5 15
+`define A6 16
+`define A7 17
+`define S2 18 // Saved register 2 - 11
+`define S3 19
+`define S4 20
+`define S5 21
+`define S6 22
+`define S7 23
+`define S8 24
+`define S9 25
+`define S10 26
+`define S11 27
+`define T3 28 // Temporary 3 - 6
+`define T4 29
+`define T5 30
+`define T6 31
+
+// Generic register mnemonics
+`define X0 0
+`define X1 1
+`define X2 2
+`define X3 3
+`define X4 4
+`define X5 5
+`define X6 6
+`define X7 7
+`define X8 8
+`define X9 9
+`define X10 10
+`define X11 11
+`define X12 12
+`define X13 13
+`define X14 14
+`define X15 15
+`define X16 16
+`define X17 17
+`define X18 18
+`define X19 19
+`define X20 20
+`define X21 21
+`define X22 22
+`define X23 23
+`define X24 24
+`define X25 25
+`define X26 26
+`define X27 27
+`define X28 28
+`define X29 29
+`define X30 30
+`define X31 31
+
